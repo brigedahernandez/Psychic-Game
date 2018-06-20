d630c472251e0e4732699cdd33dc26400c2cbc22
Brigeda Hernandez <brigedahernandez@gmail.com>
2018-06-20 17:57:48 -0500

    updatiest version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-
+//
 
 var UserGuess = null;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,14 +8,12 @@ var lost = 0;
 var AlreadyGuessed = [];
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+var UserGuess = event.key;
+
 document.onkeyup = function (event) {
 
-    if (GuessesLeft === 0) {
-        return;
-    }
-}
 
-var UserGuess = event.key;
+
 
 if ((UserGuess === randomLetter))
     won++;
@@ -23,3 +21,8 @@ if ((UserGuess === randomLetter))
 if ((UserGuess != randomLetter))
     lost++;
 GuessesLeft--;
+
+if (GuessesLeft === 0) {
+    return;
+}
+}
