3e20ff36ce32ade56f795da7f78416b22bf3be7d
Brigeda Hernandez <brigedahernandez@gmail.com>
2018-06-20 04:15:04 -0500

    insomnia

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,25 @@
-var UserGuess = "null" ;
-var  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var GuessNumber = "0" ;
-var won = "0" ;
-var lost = "0" ;
-var AlreadyGuessed = "[]" ;
+
+
+var UserGuess = null;
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var GuessesLeft = 8;
+var won = 0;
+var lost = 0;
+var AlreadyGuessed = [];
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-document.onkeyup = function(event) {}
+document.onkeyup = function (event) {
+
+    if (GuessesLeft === 0) {
+        return;
+    }
+}
 
 var UserGuess = event.key;
 
 if ((UserGuess === randomLetter))
-won++;
+    won++;
 
 if ((UserGuess != randomLetter))
-lost++;
-
-
-
+    lost++;
+GuessesLeft--;
